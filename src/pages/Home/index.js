93482dd34bdb8446885df6baa900e36671710e19
Le Nhut Hao <haole01627@gmail.com>
2021-10-20 16:38:53 +0700

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Data</h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Home Datasadsdsdsdasd</h1>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
